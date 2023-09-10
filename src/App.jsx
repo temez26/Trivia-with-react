27fac89520a2ec414b5559491370baaf281e2c1d
Teemu Kalmari <Teemu.Kalmari@student.lab.fi>
2023-09-10 22:00:51 +0300

    added answer selection 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { faDiceSix } from "@fortawesome/free-solid-svg-icons";
-import { faDiamond, faWandMagicSparkles, faXmark, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faDiamond, faWandMagicSparkles, faXmark, faCheck, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import React, { useState, useEffect } from 'react';
 import he from 'he'; // Import the 'he' library for HTML entity decoding
 
 function App() {
     const [question, setQuestion] = useState('');
-    const [answer, setAnswer] = useState('');
     const [correctAnswer, setCorrectAnswer] = useState('');
     const [showCorrectAnswer, setShowCorrectAnswer] = useState(false);
     const [congratulations, setCongratulations] = useState('');
     const [correctCount, setCorrectCount] = useState(0); // Track correct answers
     const [incorrectCount, setIncorrectCount] = useState(0); // Track incorrect answers
+    const [options, setOptions] = useState([]);
+    const [answered, setAnswered] = useState(false);
 
     const decodeHtmlEntities = (html) => {
         return he.decode(html);
@@ -20,13 +21,17 @@ function App() {
 
     const fetchRandomQuestion = async () => {
         try {
-            const response = await fetch('https://opentdb.com/api.php?amount=1');
+            setAnswered(false); // Reset answered state
+            const response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple');
             const data = await response.json();
-            const randomQuestion = decodeHtmlEntities(data.results[0].question); // Decode HTML entities
-            const correctAnswer = decodeHtmlEntities(data.results[0].correct_answer); // Decode HTML entities
+            const randomQuestion = decodeHtmlEntities(data.results[0].question);
+            const correctAnswer = decodeHtmlEntities(data.results[0].correct_answer);
+            const incorrectAnswers = data.results[0].incorrect_answers.map(decodeHtmlEntities);
+            const options = [correctAnswer, ...incorrectAnswers];
+            options.sort(() => Math.random() - 0.5); // Shuffle the options
             setQuestion(randomQuestion);
-            setAnswer('');
             setCorrectAnswer(correctAnswer);
+            setOptions(options);
             setShowCorrectAnswer(false);
             setCongratulations('');
         } catch (error) {
@@ -34,25 +39,30 @@ function App() {
         }
     };
 
-    const handleAnswerChange = (event) => {
-        setAnswer(event.target.value);
-    };
-
-    const checkAnswer = () => {
-        if (answer.toLowerCase() === correctAnswer.toLowerCase()) {
-            setCongratulations('Congratulations, correct answer!');
-            setShowCorrectAnswer(false);
-            setCorrectCount(correctCount + 1); // Increment correct count
-        } else {
-            setShowCorrectAnswer(true);
-            setCongratulations('');
-            setIncorrectCount(incorrectCount + 1); // Increment incorrect count
+    const checkAnswer = (selectedOption) => {
+        if (!answered) {
+            if (selectedOption.toLowerCase() === correctAnswer.toLowerCase()) {
+                setCongratulations('Congratulations, correct answer !!!');
+                setShowCorrectAnswer(false);
+                setCorrectCount(correctCount + 1);
+            } else {
+                setShowCorrectAnswer(true);
+                setCongratulations('');
+                setIncorrectCount(incorrectCount + 1);
+            }
+            setAnswered(true); // Set answered to true after the user answers
         }
     };
 
-    // Calculate the percentage of correct answers
-    const totalQuestions = correctCount + incorrectCount;
-    const percentage = totalQuestions > 0 ? (correctCount / totalQuestions) * 100 : 0;
+    const resetGame = () => {
+        setCorrectCount(0);
+        setIncorrectCount(0);
+        fetchRandomQuestion(); // Start a new game by fetching the first question
+    };
+
+    const nextQuestion = () => {
+        fetchRandomQuestion(); // Load the next question
+    };
 
     useEffect(() => {
         fetchRandomQuestion();
@@ -68,27 +78,32 @@ function App() {
                         <p dangerouslySetInnerHTML={{ __html: question }}></p>
                     </div>
                     <div className="container3">
-                        <div className="answer">
-                            <input
-                                type="text"
-                                placeholder="Type your answer"
-                                value={answer}
-                                onChange={handleAnswerChange}
-                            />
-                            <button onClick={checkAnswer}> <FontAwesomeIcon icon={faDiamond} className="dice" />Check Answer <FontAwesomeIcon icon={faDiamond} className="dice" /></button>
+                        <div className="options">
+                            {options.map((option, index) => (
+                                <button
+                                    key={index}
+                                    onClick={() => checkAnswer(option)}
+                                    disabled={answered}
+                                >
+                                    {option}
+                                </button>
+                            ))}
                         </div>
+                        <div className="answer"></div>
                         {showCorrectAnswer && (
                             <div className="correct-answer">
-                                <p>Correct Answer: {correctAnswer}</p>
+                                <p>Wrong <FontAwesomeIcon icon={faXmark} style={{ color: "#cc0000", }} /> = Correct Answer: {correctAnswer} </p>
                             </div>
                         )}
                         <div className="congratulations">
-                            <p>{congratulations}</p>
+                            <p>{congratulations} </p>
                         </div>
                         <p>Correct: {correctCount}  <FontAwesomeIcon icon={faCheck} style={{ color: "#00a803", }} /></p>
                         <p>  Incorrect: {incorrectCount}         <FontAwesomeIcon icon={faXmark} style={{ color: "#cc0000", }} /></p>
-                        <p>Percentage: {percentage.toFixed(2)}%</p>
-                        <button onClick={fetchRandomQuestion}> <FontAwesomeIcon icon={faDiceSix} className="dice" beat />Get Random Question</button>
+                        <div className="next">
+                        <button onClick={nextQuestion}>Next Question <FontAwesomeIcon icon={faChevronRight} bounce /></button>
+                        </div>
+                        <button onClick={resetGame}> <FontAwesomeIcon  icon={faDiceSix} className="dice" beat />Reset Game</button>
                         <div className="icon">
                             <FontAwesomeIcon icon={faCoffee} />
                         </div>

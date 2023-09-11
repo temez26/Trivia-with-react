f7c2f525b32ed48a15c1b0a438d0edb82d8314cf
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2023-09-11 11:04:28 +0300

    minor changes to layout

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Footer from './footer.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.min.js'; // Import Bootstrap JavaScript
+
+
+import './App.css'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Footer/>
+  </React.StrictMode>,
+)
